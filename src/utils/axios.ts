c7d626587a238f728205fb7a04e1a35a5ea3ba0f
syedslegend786 <syedslegend786@gmail.com>
2023-11-22 17:40:23 +0500

    added base url to axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,5 +2,5 @@ import Axios from "axios"
 
 const backendUrl = process.env.BACKEND_URL as string
 export const axios = Axios.create({
-  baseURL: backendUrl,
+  baseURL: "https://salleh-assessment.vercel.app/api",
 })
